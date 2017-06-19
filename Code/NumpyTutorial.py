009fe00edb0de156de199add9e3b4b086ba7e85e
begovic2 <begovic2@illinois.edu>
2017-06-19 10:06:31 -0500

    Progress with array indexing in NumPy

--- Code/NumpyTutorial.py
+++ Code/NumpyTutorial.py
@@ -44,11 +44,13 @@ a[1, 2] = -15
 print(a)
 print(b)
 
+
 a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 # Two ways of accessing the data in the middle row of the array.
 # Mixing integer indexing with slices yields an array of lower rank,
 # while using only slices yields an array of the same rank as the
 # original array:
+print(a)
 row_r1 = a[1, :]    # Rank 1 view of the second row of a
 row_r2 = a[1:2, :]  # Rank 2 view of the second row of a
 print(row_r1, row_r1.shape)  # Prints "[5 6 7 8] (4,)"
@@ -59,3 +61,62 @@ col_r1 = a[:, 1]
 col_r2 = a[:, 1:2]
 print(col_r1, col_r1.shape)  # Prints "[ 2  6 10] (3,)"
 print(col_r2, col_r2.shape)
+
+
+a = np.array([[1, 2], [3, 4], [5, 6]])
+print(a)
+# An example of integer array indexing.
+# The returned array will have shape (3,) and
+print(a[[0, 1, 2], [0, 1, 0]])  # Prints "[1 4 5]"
+
+# The above example of integer array indexing is equivalent to this:
+print(np.array([a[0, 0], a[1, 1], a[2, 0]]))  # Prints "[1 4 5]"
+
+# When using integer array indexing, you can reuse the same
+# element from the source array:
+print(a[[0, 0], [1, 1]])  # Prints "[2 2]"
+
+# Equivalent to the previous integer array indexing example
+print(np.array([a[0, 1], a[0, 1]]))  # Prints "[2 2]"
+# Create a new array from which we will select elements
+
+
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+print(a)  # prints "array([[ 1,  2,  3],
+          #                [ 4,  5,  6],
+          #                [ 7,  8,  9],
+          #                [10, 11, 12]])"
+
+# Create an array of indices
+b = np.array([0, 2, 0, 1])
+
+# Select one element from each row of a using the indices in b
+print(a[np.arange(4), b])  # Prints "[ 1  6  7 11]"
+
+# Mutate one element from each row of a using the indices in b
+a[np.arange(4), b] += 10
+
+print(a)  # prints "array([[11,  2,  3],
+          #                [ 4,  5, 16],
+          #                [17,  8,  9],
+          #                [10, 21, 12]])
+
+
+a = np.array([[1, 2], [3, 4], [5, 6]])
+bool_idx = (a > 2)   # Find the elements of a that are bigger than 2;
+                     # this returns a numpy array of Booleans of the same
+                     # shape as a, where each slot of bool_idx tells
+                     # whether that element of a is > 2.
+
+print(bool_idx)      # Prints "[[False False]
+                     #          [ True  True]
+                     #          [ True  True]]"
+
+# We use boolean array indexing to construct a rank 1 array
+# consisting of the elements of a corresponding to the True values
+# of bool_idx
+print(a[bool_idx])  # Prints "[3 4 5 6]"
+
+# We can do all of the above in a single concise statement:
+print(a[a > 2])     # Prints "[3 4 5 6]"
+
