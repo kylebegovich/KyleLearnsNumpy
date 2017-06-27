edf1b3016cb8aaa43ab4780c827ae5ad22498993
begovic2 <begovic2@illinois.edu>
2017-06-27 09:01:24 -0500

    Learning broadcasting in Numpy

--- Code/NumpyTutorial.py
+++ Code/NumpyTutorial.py
@@ -183,3 +183,54 @@ print(x.T)
 v = np.array([1, 2, 3])
 print(v)
 print(v.T)
+
+
+# Broadcasting
+# We will add the vector v to each row of the matrix x, storing the result in the matrix y
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+y = np.empty_like(x)   # Create an empty matrix with the same shape as x
+
+# Add the vector v to each row of the matrix x with an explicit loop
+for i in range(4):
+    y[i, :] = x[i, :] + v
+
+# Now y is the following
+# [[ 2  2  4]
+#  [ 5  5  7]
+#  [ 8  8 10]
+#  [11 11 13]]
+print(y)
+
+# We will add the vector v to each row of the matrix x,
+# storing the result in the matrix y
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+vv = np.tile(v, (4, 1))   # Stack 4 copies of v on top of each other
+print(vv)                 # Prints "[[1 0 1]
+                          #          [1 0 1]
+                          #          [1 0 1]
+                          #          [1 0 1]]"
+y = x + vv  # Add x and vv elementwise
+print(y)  # Prints "[[ 2  2  4
+          #          [ 5  5  7]
+          #          [ 8  8 10]
+          #          [11 11 13]]"
+
+# We will add the vector v to each row of the matrix x,
+# storing the result in the matrix y
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+y = x + v  # Add v to each row of x using broadcasting
+print(y)  # Prints "[[ 2  2  4]
+          #          [ 5  5  7]
+          #          [ 8  8 10]
+          #          [11 11 13]]"
+
+# Broadcasting two arrays together follows these rules:
+
+# If the arrays do not have the same rank, prepend the shape of the lower rank array with 1s until both shapes have the same length.
+# The two arrays are said to be compatible in a dimension if they have the same size in the dimension, or if one of the arrays has size 1 in that dimension.
+# The arrays can be broadcast together if they are compatible in all dimensions.
+# After broadcasting, each array behaves as if it had shape equal to the elementwise maximum of shapes of the two input arrays.
+# In any dimension where one array had size 1 and the other array had size greater than 1, the first array behaves as if it were copied along that dimension
